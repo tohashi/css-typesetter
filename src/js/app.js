0548849b1a0adb1988116dd14605bb698e02706a
tohashi <dots64if@gmail.com>
2015-10-17 23:55:14 +0900

    Add react-draggable

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 
 class DocEditor extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      previewWidth: 720,
+      previewHeight: 0
+    }
+  }
+
+  componentDidMount() {
+    const img = document.createElement('img');
+    img.src = './src/img/sample.png';
+    img.onload = ((e) => {
+      this.setState({
+        previewHeight: ((this.state.previewWidth/ e.target.width) * e.target.height) || 0
+      });
+    });
+  }
+
   render() {
+    const style = {
+      width: `${this.state.previewWidth}px`,
+      height: `${this.state.previewHeight}px`
+    };
+
     return (
       <div>
-        editor
-        <img src="./src/img/sample.png" />
+        <div className="doc-image" style={style}>
+          <Draggable
+            axis="both"
+            start={{ x: 0, y: 0 }}>
+
+            <div>testtest</div>
+          </Draggable>
+        </div>
       </div>
     );
   }
