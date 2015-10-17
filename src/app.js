79fe20f378c7c9e48d5025954a81c307682223e3
tohashi <dots64if@gmail.com>
2015-10-17 19:08:47 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class DocEditor extends React.Component {
+  render() {
+    return (
+      <div>
+        editor
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<DocEditor />, document.getElementById('root'));
+
