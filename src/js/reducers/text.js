8f7b0d5f2ffdc9615d3ec96b37e231ea86aac62e
tohashi <dots64if@gmail.com>
2016-01-12 02:10:25 +0900

    undoable

--- src/js/reducers/text.js
+++ src/js/reducers/text.js
@@ -36,66 +36,80 @@ function loadFromLS() {
   return JSON.parse(localStorage.getItem('texts'));
 }
 
-const initialState = getInitialState();
-
-const reduce = (state, action) => {
-  let texts = [];
-  switch(action.type) {
-  case ActionTypes.UPDATE_TEXT:
-    const params = action.params;
-    let added = true;
-    texts = state.texts.map((text) => {
-      if (text.key === params.key) {
-        text = _.defaults(params, text);
-        added = false;
-      }
-      return text;
-    });
-    if (added) {
-      texts.push(params);
-    }
-    return _.extend({}, state, { texts });
-  case ActionTypes.REMOVE_TEXT:
-    texts = state.texts.filter((text) => {
-      return text.key !== action.key;
-    });
-    return _.extend({}, state, { texts });
-  case ActionTypes.COPY_TEXT:
-    const text = _.cloneDeep(state.texts.find((text) => {
-      return text.key === action.key;
-    }));
-    text.key = _.uniqueId(`${text.key}-`);
-    texts = state.texts;
-    texts.push(text);
-    return _.extend({}, state, { texts });
-  case ActionTypes.CLEAR_TEXTS:
-    return _.extend({}, state, { texts });
-  case ActionTypes.IMPORT_TEXTS:
-    try {
-      texts = JSON.parse(action.json);
-    } catch (e) {
-      throw Error('parse error');
-    }
-    return _.extend({}, state, { texts });
-  case ActionTypes.UNDO:
-    state.historyIdx -= 1;
-    state.texts = _.cloneDeep(history[state.historyIdx]);
-    return state;
-  case ActionTypes.REDO:
-    state.historyIdx += 1;
-    state.texts = _.cloneDeep(history[state.historyIdx]);
-    return state;
-  default:
-    return state;
-  }
+function isUndoable(historyIdx, history) {
+  return history.length > 1 && historyIdx > 0;
 }
 
+function isRedoable(historyIdx, history) {
+  return historyIdx !== Math.max(history.length - 1, 0);
+}
+
+const initialState = getInitialState();
+
 export default function texts(state = initialState, action) {
-  const modifiedState = _.extend({}, reduce(state, action), {
-    undoable: state.history.length > 1 && state.historyIdx > 0,
-    redoable: state.historyIdx !== Math.max(state.history.length - 1, 0)
-  });
+  if (_.contains([ActionTypes.UNDO, ActionTypes.REDO], action.type)) {
+    const historyIdx = action.type === ActionTypes.UNDO ?
+      state.historyIdx - 1 : state.historyIdx + 1;
+    return _.extend({}, state, {
+      historyIdx,
+      texts: _.cloneDeep(state.history[historyIdx]),
+      undoable: isUndoable(historyIdx, state.history),
+      redoable: isRedoable(historyIdx, state.history)
+    });
+  }
+
+  let modifiedState = (() => {
+    let texts = [];
+    switch(action.type) {
+    case ActionTypes.UPDATE_TEXT:
+      const params = action.params;
+      let added = true;
+      texts = state.texts.map((text) => {
+        if (text.key === params.key) {
+          text = _.defaults(params, text);
+          added = false;
+        }
+        return text;
+      });
+      if (added) {
+        texts.push(params);
+      }
+      return _.extend({}, state, { texts });
+    case ActionTypes.REMOVE_TEXT:
+      texts = state.texts.filter((text) => {
+        return text.key !== action.key;
+      });
+      return _.extend({}, state, { texts });
+    case ActionTypes.COPY_TEXT:
+      const text = _.cloneDeep(state.texts.find((text) => {
+        return text.key === action.key;
+      }));
+      text.key = _.uniqueId(`${text.key}-`);
+      texts = state.texts;
+      texts.push(text);
+      return _.extend({}, state, { texts });
+    case ActionTypes.CLEAR_TEXTS:
+      return _.extend({}, state, { texts });
+    case ActionTypes.IMPORT_TEXTS:
+      try {
+        texts = JSON.parse(action.json);
+      } catch (e) {
+        throw Error('parse error');
+      }
+      return _.extend({}, state, { texts });
+    }
+  })();
+
+  if (!modifiedState) {
+    return state;
+  }
   saveToLS(modifiedState.texts);
-  return modifiedState;
+  modifiedState.history.push(_.cloneDeep(modifiedState.texts));
+  modifiedState.historyIdx = modifiedState.history.length - 1;
+
+  return _.extend({}, modifiedState, {
+    undoable: isUndoable(modifiedState.historyIdx, modifiedState.history),
+    redoable: isRedoable(modifiedState.historyIdx, modifiedState.history)
+  });
 }
 
